ba35dfc97571d3f38c420beb63608c903a8bb5ae
Callum Stott <cstott@pivotal.io>
2017-12-08 16:02:59 +0000

    Add deployment pipeline and config files

---
+++ deployment/pws/config/config.js
@@ -0,0 +1,15 @@
+window.Retro = {
+  config: {
+    "globalNamespace": "Retro",
+    "title": "Postfacto",
+    "scripts": ["application.js"],
+    "stylesheets": ["application.css"],
+    "useRevManifest": true,
+
+    "api_base_url": "https://{{api-app-name}}.cfapps.io",
+    "websocket_url": "wss://{{api-app-name}}.cfapps.io:4443/cable",
+    "contact": "",
+    "terms": "",
+    "privacy": ""
+  }
+}
