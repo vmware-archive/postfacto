f86be08f75219e063528c68487cf3a59dfa51761
Callum Stott <cstott@pivotal.io>
2018-02-09 17:52:06 +0000

    Add PWS deployment script

---
+++ deployment/pws/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+cf push -f deployment/pws/config/manifest-api.yml -p api
+cf run-task api-app-name 'ADMIN_EMAIL=email@example.com ADMIN_PASSWORD=password rake admin:create_user'
+
+pushd web
+  NODE_ENV=production gulp assets
+  gulp package
+  cp ../deployment/pws/config/config.js package
+popd
+
+cf push -f deployment/pws/config/manifest-web.yml -p web/package
