a41e1d61d3d166c578112aa084a44ec60a25feec
Callum Stott <callum@seadowg.com>
2018-03-09 11:45:46 +0000

    Remove terms feature flags

--- api/app/controllers/config_controller.rb
+++ api/app/controllers/config_controller.rb
@@ -31,8 +31,6 @@
 class ConfigController < ApplicationController
   def show
     render json: {
-      new_terms: Time.parse("#{NEW_TERMS_RELEASE} UTC") < CLOCK.current_time,
-      old_terms: Time.parse("#{OLD_TERMS_REMOVAL} UTC") > CLOCK.current_time,
       archive_emails: ARCHIVE_EMAILS
     }
   end
