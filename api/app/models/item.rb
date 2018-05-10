20cfee6826f9775f36e4e2de7149db2782f8e420
Callum Stott <callum@seadowg.com>
2018-05-10 09:46:30 +0100

    Check SQLite3Adapter is defined to avoid explosions in production

--- api/app/models/item.rb
+++ api/app/models/item.rb
@@ -53,6 +53,7 @@ class Item < ActiveRecord::Base
   end
 
   def using_sqlite?
+    return false unless defined? ActiveRecord::ConnectionAdapters::SQLite3Adapter
     ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::SQLite3Adapter
   end
 end
