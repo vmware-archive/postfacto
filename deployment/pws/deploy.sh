4c86825e689f1fb3b836260ab741f3e21135c767
Daniel Garnier-Moiroux <dgarnier-moiroux@pivotal.io>
2018-07-18 15:45:04 +0200

    Update and fix the PWS deploy script
    - The script can be run from anywhere (no relative paths)
    - The "package" directory within assets/web does not exist in the
      release zip, so we removed this
    
    Signed-off-by: Jieke Pan <jpan@mobiquityinc.com>

--- deployment/pws/deploy.sh
+++ deployment/pws/deploy.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-cf push -f config/manifest-api.yml -p api
+# The directory in which this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cf push -f config/manifest-api.yml -p "$SCRIPT_DIR"/assets/api
 cf run-task {{api-app-name}} 'ADMIN_EMAIL=email@example.com ADMIN_PASSWORD=password rake admin:create_user'
 
-cp config/config.js package
-cf push -f config/manifest-web.yml -p web/package
+cp config/config.js "$SCRIPT_DIR"/assets/web
+cf push -f config/manifest-web.yml -p "$SCRIPT_DIR"/assets/web
