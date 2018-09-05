66a6f0f280abc661a93c0ffa770db9dc58ed9a95
Mike Stallard <mike@autolist.com>
2018-09-04 19:52:52 -0700

    Add support for ACTION_CABLE_HOST override in environment
    If you:
    
    1) Do not have `VCAP_APPLICATION` available to you in your deployed
    environment (for instance on Heroku)
    2) Want to provide a manual override for the hostname used for
    ActionCable configuration
    
    Then you can now do so with the ACTION_CABLE_HOST environment variable.

---
+++ api/lib/configurations/action_cable_host_provider.rb
@@ -0,0 +1,37 @@
+#
+# Postfacto, a free, open-source and self-hosted retro tool aimed at helping
+# remote teams.
+#
+# Copyright (C) 2016 - Present Pivotal Software, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+#
+# it under the terms of the GNU Affero General Public License as
+#
+# published by the Free Software Foundation, either version 3 of the
+#
+# License, or (at your option) any later version.
+#
+#
+#
+# This program is distributed in the hope that it will be useful,
+#
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+#
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#
+# GNU Affero General Public License for more details.
+#
+#
+#
+# You should have received a copy of the GNU Affero General Public License
+#
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+class ActionCableHostProvider
+  def host
+    return ENV['ACTION_CABLE_HOST'] unless ENV['ACTION_CABLE_HOST'].nil?
+
+    JSON.parse(ENV['VCAP_APPLICATION']).fetch('uris')[0] unless ENV['VCAP_APPLICATION'].nil?
+  end
+end
---
+++ api/spec/lib/action_cable_configuration_provider_spec.rb
@@ -0,0 +1,67 @@
+#
+# Postfacto, a free, open-source and self-hosted retro tool aimed at helping
+# remote teams.
+#
+# Copyright (C) 2016 - Present Pivotal Software, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+#
+# it under the terms of the GNU Affero General Public License as
+#
+# published by the Free Software Foundation, either version 3 of the
+#
+# License, or (at your option) any later version.
+#
+#
+#
+# This program is distributed in the hope that it will be useful,
+#
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+#
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#
+# GNU Affero General Public License for more details.
+#
+#
+#
+# You should have received a copy of the GNU Affero General Public License
+#
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+require 'action_cable_configuration_provider'
+require 'climate_control'
+
+describe ActionCableConfigurationProvider do
+  subject { ActionCableConfigurationProvider.new }
+
+  describe '#config' do
+    let(:client_origin) { 'fixture-client-origin' }
+
+    around do |example|
+      ClimateControl.modify('WEBSOCKET_PORT' => websocket_port, 'CLIENT_ORIGIN' => client_origin) do
+        example.run
+      end
+    end
+
+    context 'when WEBSOCKET_PORT is set in the environment' do
+      let(:websocket_port) { 'fixture-port' }
+      it 'uses that value in action cable url' do
+        configuration = subject.config(host: 'fixture-host')
+
+        expect(configuration.url).to eq('wss://fixture-host:fixture-port/cable')
+        expect(configuration.allowed_request_origins).to eq(['https://fixture-host', 'fixture-client-origin'])
+      end
+    end
+
+    context 'when WEBSOCKET_PORT is NOT set in the environment' do
+      let(:websocket_port) { nil }
+
+      it 'defaults to port 443' do
+        configuration = subject.config(host: 'fixture-host')
+
+        expect(configuration.url).to eq('wss://fixture-host:443/cable')
+        expect(configuration.allowed_request_origins).to eq(['https://fixture-host', 'fixture-client-origin'])
+      end
+    end
+  end
+end
