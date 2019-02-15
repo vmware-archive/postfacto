236c76374416ea2a50c3b9a4c7183378361f75a5
David Evans <devans@pivotal.io>
2019-02-15 14:11:56 +0000

    Fix quoting inside tmux and only kill the current session when ending

--- run.sh
+++ run.sh
@@ -32,22 +32,24 @@ pushd "$BASE_DIR/api" >/dev/null
   ADMIN_EMAIL="$ADMIN_USER" ADMIN_PASSWORD="$ADMIN_PASS" rake admin:create_user
 popd >/dev/null
 
+export BASE_DIR
+export RAILS_ENV
 export USE_MOCK_GOOGLE
 export GOOGLE_OAUTH_CLIENT_ID
 
 # Launch content server for frontend
 
-TMUX_COMMAND="tmux new-session npm --prefix='$BASE_DIR/web' start"
+TMUX_COMMAND="tmux new-session -s postfacto_run npm --prefix=\"\$BASE_DIR/web\" start"
 
 # Launch API
 
-TMUX_COMMAND+=" \; split-window -v /bin/bash -c 'cd $BASE_DIR/api && echo \"API\" && bundle exec rails server -b 0.0.0.0 -p 4000 -e '\"'\"'$RAILS_ENV'\"'\"''"
+TMUX_COMMAND+=" \; split-window -v /bin/bash -c 'cd \"\$BASE_DIR/api\" && echo \"API\" && bundle exec rails server -b 0.0.0.0 -p 4000 -e \"\$RAILS_ENV\"'"
 
 # Launch mock auth server if needed
 
 if [[ "$USE_MOCK_GOOGLE" == "true" ]]; then
   export GOOGLE_AUTH_ENDPOINT="http://localhost:3100/auth"
-  TMUX_COMMAND+=" \; split-window -v npm --prefix='$BASE_DIR/mock-google-server' start"
+  TMUX_COMMAND+=" \; split-window -v npm --prefix=\"\$BASE_DIR/mock-google-server\" start"
 fi
 
 INFO+=$'\n'
@@ -57,5 +59,8 @@ INFO+="App will be available at http://localhost:3000/"$'\n'
 INFO+="Press 'q' to stop all services"
 export INFO
 
-TMUX_COMMAND+=" \; split-window -v /bin/bash -c 'echo \"\$INFO\" | less; tmux kill-server'"
+clear
+
+TMUX_COMMAND+=" \; split-window -v /bin/bash -c 'echo \"\$INFO\" | less; tmux kill-session -t postfacto_run'"
+TMUX_COMMAND+=" \; select-layout even-vertical"
 /bin/bash -c "$TMUX_COMMAND"
