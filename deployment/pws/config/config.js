cae6a76a05d30621f3d17040d54c6cfe12c5e4ce
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2019-05-16 16:13:45 +0200

    Replace placeholders for app and system domain
    Introduce separation between system domain (API endpoint) and app domain. It
    also replaces `{{pcf-url}}` in `deployment/pws/config/config.js` with provided
    app domain.
    
    Fixes #134.
    
    Signed-off-by: Matthias Diester <matthias.diester@de.ibm.com>

--- deployment/pws/config/config.js
+++ deployment/pws/config/config.js
@@ -5,8 +5,8 @@ window.Retro = {
     "scripts": ["application.js"],
     "stylesheets": ["application.css"],
     "useRevManifest": true,
-    "api_base_url": "https://{{api-app-name}}.cfapps.io",
-    "websocket_url": "wss://{{api-app-name}}.cfapps.io:4443/cable",
+    "api_base_url": "https://{{api-app-name}}.{{pcf-url}}",
+    "websocket_url": "wss://{{api-app-name}}.{{pcf-url}}:4443/cable",
     "contact": "",
     "terms": "",
     "privacy": ""
