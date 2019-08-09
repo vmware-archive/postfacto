0d94c0c2d97cc6f79fc69c46412a7c9bc8b42488
Jonathan Sharpe <jsharpe@pivotal.io>
2019-08-09 15:13:02 +0100

    Update deploy and upgrade scripts to work on Unix (#176)
    * Fix sed usage in Heroku upgrade/deploy scripts
    
    * Fix sed usage in CF upgrade script
    
    * Check existing buildpack before attempting to update it

--- deployment/upgrade-cf.sh
+++ deployment/upgrade-cf.sh
@@ -4,7 +4,8 @@ set -e
 
 WEB_HOST=$1
 API_HOST=$2
-CF_URL=${3:-https://api.run.pivotal.io}
+API_ENDPOINT=${3:-https://api.run.pivotal.io}
+APP_DOMAIN=${4:-cfapps.io}
 SESSION_TIME=${SESSION_TIME:-'""'}
 
 # The directory in which this script is located
@@ -12,10 +13,14 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ASSETS_DIR="$SCRIPT_DIR"/assets
 CONFIG_DIR="$SCRIPT_DIR"/config
 
-cf login -a $CF_URL
+cf login -a $API_ENDPOINT
 
 cf push -f "$CONFIG_DIR"/manifest-api.yml -p "$ASSETS_DIR"/api --var api-app-name=$API_HOST --var web-app-name=$WEB_HOST --var pcf-url=$CF_URL --var session-time=$SESSION_TIME
 
-sed -i '' "s/{{api-app-name}}/${API_HOST}/" "$CONFIG_DIR"/config.js
-cp "$CONFIG_DIR"/config.js "$ASSETS_DIR"/web
+sed \
+  -e "s/{{api-app-name}}/${API_HOST}/" \
+  -e "s/{{pcf-url}}/${APP_DOMAIN}/" \
+  <"$CONFIG_DIR"/config.js \
+  >"$ASSETS_DIR"/web/config.js
+
 cf push -f "$CONFIG_DIR"/manifest-web.yml -p "$ASSETS_DIR"/web --var api-app-name=$API_HOST --var web-app-name=$WEB_HOST
