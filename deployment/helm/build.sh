fb93140d8f3066d2efded4768be110a047a19eb1
Chris Whitty <1538281+crswty@users.noreply.github.com>
2019-10-26 22:13:47 +0100

    Build helm chart in CI

--- deployment/helm/build.sh
+++ deployment/helm/build.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 set -e
 
+VERSION=$1
+
+if [ $# -lt 1 ]; then
+  echo "usage: ./build.sh <version>"
+  echo "Builds the chart writing the given version as the app and chart version"
+  exit 1
+fi
+
+sed -i.bak s/version:.*/version:\ $VERSION/  Chart.yaml
+sed -i.bak s/appVersion:.*/appVersion:\ $VERSION/  Chart.yaml
+
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 
-rm Chart.lock # workaround for: https://github.com/helm/helm/issues/6416
+rm -f Chart.lock # workaround for: https://github.com/helm/helm/issues/6416
 helm dependency build
 
 helm package .
