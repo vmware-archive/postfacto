c8b227a4d033aafd97a80ed9c3c5e5f135351c2c
Jonathan Sharpe <jsharpe@pivotal.io>
2021-04-28 18:16:36 +0100

    Try publishing and testing package

--- download-draft-package.sh
+++ download-draft-package.sh
@@ -32,14 +32,16 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Get the asset url of the latest draft
-asset_url=$(curl -u $GITHUB_USERNAME:$ACCESS_TOKEN \
+# TODO: need to switch this back to pivotal/postfacto
+asset_url=$(curl \
+          --header "Authorization: Bearer $GITHUB_TOKEN" \
           --header "Accept: application/json" \
-          https://api.github.com/repos/pivotal/postfacto/releases \
-          | jq --raw-output '[.[] | select(.draft==true)][0].assets[0].url')
+          https://api.github.com/repos/textbook/postfacto/releases \
+          | jq --raw-output '[[.[] | select(.draft==true)] | first.assets | .[] | select(.name=="package.zip")] | first.url')
 
 # Download asset
 curl --location --silent --show-error \
-          -u $GITHUB_USERNAME:$ACCESS_TOKEN \
+          --header "Authorization: Bearer $GITHUB_TOKEN" \
           --header "Accept: application/octet-stream" \
           --output $SCRIPT_DIR/package.zip \
           $asset_url
